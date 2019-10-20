732e7b59d7c83e2353fdf0eb3118a4bff8a35061
Louis Alonzo <Louis.A@live.com>
2019-10-20 02:13:15 -0700

    rewrote jQuery function to save input on each time slot

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-$('#btn').on('click', function() {
-  let time = $(this).parent().parent().attr('id');
+// event.preventDefault() 
 
+//creating an event upon click. a function is called for
+$('.choice').on('click', function() {
+    event.preventDefault();
+// creating variable to hold id of element tag
+  let time = $(this).parent().parent().attr('id');
+//creating variable to hold the value of user input
   let value = $(this).parent().parent().find(".description").val();
+// console log responses to check values of clicks
 console.log(time);
 console.log(value);
-  localStorage.setItem(time, value)
+// name/value pairs are set to local storage 
+  localStorage.setItem(time, value);
 
 })
 
-var tableRow = $(this).attr("id");
+// moment.js to add date/time to webpage
+$("#currentDay").append(moment().format('LLLL'));
 
-console.log(tableRow);
 
+
+// retrieving local storage name/value pairs and appending to relative time slot
 $("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
